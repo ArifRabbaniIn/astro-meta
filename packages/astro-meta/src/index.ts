14e04b85e37e0a467ddf55042c899236d8c98949
Arif Rabbani <arif.93rabbani@gmail.com>
2025-02-28 11:19:01 +0530

    feat(astro-meta): add entrypoint

---
+++ packages/astro-meta/src/index.ts
@@ -0,0 +1,7 @@
+import MetaTags from './components/MetaTags.astro';
+
+export * from './types/twitter-metadata';
+export * from './types/open-graph-metadata';
+export * from './types/metadata';
+
+export { MetaTags };
