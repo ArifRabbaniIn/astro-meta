67185c9973dd3aa2a044d6a4ffa6b270a9e45066
Arif Rabbani <arif.93rabbani@gmail.com>
2025-03-07 22:43:36 +0530

    docs: configure starlight-typedoc

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import starlightTypeDoc, { typeDocSidebarGroup } from 'starlight-typedoc';
 
 // https://astro.build/config
 export default defineConfig({
@@ -26,6 +27,13 @@ export default defineConfig({
           label: 'Reference',
           autogenerate: { directory: 'reference', collapsed: true },
         },
+        typeDocSidebarGroup,
+      ],
+      plugins: [
+        starlightTypeDoc({
+          entryPoints: ['../package/src/type.ts'],
+          tsconfig: '../package/tsconfig.json',
+        }),
       ],
     }),
   ],
