0950906aa1c5a4453fba3a81d962a00a786cec69
Arif Rabbani <arif.93rabbani@gmail.com>
2025-03-09 16:22:25 +0530

    package(open-graph): add type for object types

---
+++ package/src/features/open-graph/types/object-type.ts
@@ -0,0 +1,16 @@
+/**
+ * Open Graph Type Categories
+ */
+export type OpenGraphType =
+  | 'website'
+  | 'article'
+  | 'book'
+  | 'profile'
+  | 'music.song'
+  | 'music.album'
+  | 'music.playlist'
+  | 'music.radio_station'
+  | 'video.movie'
+  | 'video.episode'
+  | 'video.tv_show'
+  | 'video.other';
