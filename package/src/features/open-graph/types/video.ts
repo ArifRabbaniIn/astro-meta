86149c3aafc2c9d6cf64c4db8ed38a46c7986a5e
Arif Rabbani <arif.93rabbani@gmail.com>
2025-03-09 16:36:56 +0530

    package(open-graph): add type for optional video property

---
+++ package/src/features/open-graph/types/video.ts
@@ -0,0 +1,39 @@
+/**
+ * Open Graph Video Metadata
+ */
+export type OpenGraphVideo = {
+  /**
+   * A URL to a video file that complements this object.
+   *
+   * @example `<meta property="og:video" content="https://example.com/movie.mp4">`
+   */
+  url: string;
+
+  /**
+   * An alternate URL to use if the webpage requires HTTPS.
+   *
+   * @example `<meta property="og:video:secure_url" content="https://secure.example.com/movie.mp4">`
+   */
+  secureUrl?: string;
+
+  /**
+   * The MIME type of the video file.
+   *
+   * @example `<meta property="og:video:type" content="video/mp4">`
+   */
+  type?: string;
+
+  /**
+   * The width of the video in pixels.
+   *
+   * @example `<meta property="og:video:width" content="400">`
+   */
+  width?: number;
+
+  /**
+   * The height of the video in pixels.
+   *
+   * @example `<meta property="og:video:height" content="300">`
+   */
+  height?: number;
+};
